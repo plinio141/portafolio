3f776f48bc7112eef204f1b813ca4c97470cf65d
Plinio Grijalba <plinio.grijalba@davincitech.co>
2021-05-04 16:29:25 -0500

    remove / in url images

--- src/templates/Skill.js
+++ src/templates/Skill.js
@@ -2,7 +2,7 @@
 const Skill = (skill) => {
     const view = `
         <div class="skill">
-            <image src="/images/${skill.icon}.png" style="width: ${skill.width}; height: ${skill.height};"/>
+            <image src="images/${skill.icon}.png" style="width: ${skill.width}; height: ${skill.height};"/>
         </div>
     `;
 
