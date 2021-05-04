55e57616b847fbcc138b3bf464a8d1d6e844d8cd
Plinio Grijalba <plinio.grijalba@davincitech.co>
2021-05-04 16:42:04 -0500

    remove / in url images profile

--- src/templates/SocialNetwork.js
+++ src/templates/SocialNetwork.js
@@ -1,7 +1,7 @@
 const SocialNetwork = (socialNetworks) => {
     const view = socialNetworks.map(socialNetwork => `
         <a href="${socialNetwork.url}" class="icon" target="_blank">
-            <image src="/images/${socialNetwork.icon}.svg" class="icon-image" />
+            <image src="images/${socialNetwork.icon}.svg" class="icon-image" />
         </a>
     `).join('');
 
